49307a377d7a9a27a19f9dce0828e6c91d41862b
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2024-07-13 18:40:31 +0100

    add more endpoint routes

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ import express from "express";
 
 // initialize express app
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
+// dummby database
 const posts = [
   {
     id: 1,
@@ -25,20 +26,37 @@ const posts = [
 
 // basic express routing
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Cohort 4 api</h1>");
+  res.send("<h1>Hello Cohort 4 api development</h1>");
 });
-app.get("/about", (req, res) => {
-  res.send("<h1>This is about us page</h1>");
-});
-app.get("/contact", (req, res) => {
-  res.send("<h1>This is contact us page</h1>");
-});
+// app.get("/about", (req, res) => {
+//   res.send("<h1>This is about us page</h1>");
+// });
+// app.get("/contact", (req, res) => {
+//   res.send("<h1>This is contact us page</h1>");
+// });
 
+/// get all posts
 app.get("/api/v1/posts", (req, res) => {
   // res.send(posts)
   res.status(200).json({
     posts,
     message: "Post successfully retrieved",
+  });
+});
+
+// get a single
+app.get("/api/v1/posts/:id", (req, res) => {
+  // console.log(req.params);
+  const id = parseInt(req.params.id);
+  res.send(posts.filter((post) => post.id === id));
+});
+
+app.delete("/api/v1/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  let deletedPost = posts.filter((post) => post.id !== id);
+  console.log(deletedPost)
+  res.json({
+    message: `Post id = ${id} successfully deleted`,
   });
 });
 
