55f589d485fadb16c100828b4e7ba48d85cfafd0
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2024-07-14 12:41:28 +0100

    create complete crud endpoints suing dummy database

--- app.js
+++ app.js
@@ -2,25 +2,35 @@ import express from "express";
 
 // initialize express app
 const app = express();
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = 5000;
+
 
 // dummby database
 const posts = [
   {
     id: 1,
     title: "my post one",
+    desc: "my lovely post",
   },
   {
     id: 2,
     title: "my post two",
+    desc: "my lovely post",
   },
   {
     id: 3,
     title: "my post three",
+    desc: "my lovely post",
   },
   {
     id: 4,
     title: "my post four",
+    desc: "my lovely post",
   },
 ];
 
@@ -44,19 +54,67 @@ app.get("/api/v1/posts", (req, res) => {
   });
 });
 
-// get a single
+// get a single post
 app.get("/api/v1/posts/:id", (req, res) => {
   // console.log(req.params);
   const id = parseInt(req.params.id);
-  res.send(posts.filter((post) => post.id === id));
+  const post = posts.find((p) => p.id === id);
+  if (!post) {
+    res.status(404).json({
+      message: `Post with the id = ${id} is not found`,
+    });
+  } else {
+    res.status(200).json({
+      post,
+      message: "Post successfull retrieved",
+    });
+  }
 });
 
+// delete a single post
 app.delete("/api/v1/posts/:id", (req, res) => {
   const id = parseInt(req.params.id);
   let deletedPost = posts.filter((post) => post.id !== id);
-  console.log(deletedPost)
-  res.json({
-    message: `Post id = ${id} successfully deleted`,
+  // console.log(deletedPost);
+  if (!deletedPost) {
+    res.status(404).json({
+      message: `Post with the id = ${id} is not found`,
+    });
+  } else {
+    res.json({
+      message: `Post id = ${id} successfully deleted`,
+    });
+  }
+});
+// create new post
+app.post("/api/v1/posts", (req, res) => {
+  // console.log(req.body)
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+    desc: req.body.desc,
+  };
+  posts.push(newPost);
+  res.status(201).json({
+    posts,
+    message: "Post successfully created",
+  });
+});
+
+// update post
+app.put("/api/v1/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id);
+  if (!post) {
+    res.status(404).json({
+      message: `Post with the id = ${id} is not found`,
+    });
+  }
+  post.title = req.body.title;
+  post.desc = req.body.desc;
+  res.status(200).json({
+    post,
+    message: "Post successfully updated",
   });
 });
 
---
+++ noted.md
@@ -0,0 +1,7 @@
+
+### Refactoring
+-----------------
+
+#### get single post route
+- use of find() to find post
+- create a condition to handle `404 error` or if no post is found    
